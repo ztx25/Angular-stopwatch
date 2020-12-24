7eec1725d68a5aa11b41a6574cdf4f846aed83d5
Vitalik <ztx25sss@gmail.com>
2020-12-24 18:14:54 +0200

    Stopwatch

---
+++ Angular-stopwatch/src/app/stopwatch/stopwatch.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { timer, fromEvent, Subscription } from 'rxjs';
+import { timeInterval } from 'rxjs/operators';
+
+let listenerCount = 0;  //удалить
+
+export interface Timer {
+  time: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+export let initialTimeState: Timer = {
+  time: 0,
+  hours: 0,
+  minutes: 0,
+  seconds: 0,
+}
+
+@Component({
+  selector: 'app-stopwatch',
+  templateUrl: './stopwatch.component.html',
+  styleUrls: ['./stopwatch.component.scss']
+})
+export class StopwatchComponent implements OnInit {
+  timeState = { ...initialTimeState }
+
+  isRunning: boolean = false;
+  betweenСlicksInterval: number = 300;
+  buttonClick: Subscription;
+
+  toggle(): void {
+    this.isRunning = !this.isRunning;
+  }
+
+  wait(event: MouseEvent): void {
+    
+    const currentLitener = ++listenerCount;  //удалить
+
+    this.buttonClick = fromEvent(event.target, 'click')
+      .pipe(timeInterval())
+        .subscribe(click => {
+          
+          console.log(`listen ${currentLitener}`)  //удалить
+
+          if (click.interval < this.betweenСlicksInterval) {
+            this.isRunning = false;
+          }
+          this.buttonClick.unsubscribe()
+        })
+  }
+
+  reset(): void {
+    this.isRunning = false;
+    this.timeState = { ...initialTimeState };
+  }
+
+  ngOnInit(): void {
+    timer(1000, 1000).subscribe(() => {
+      if(this.isRunning) {
+        this.timeState.time++;
+        this.timeState.seconds = Math.floor(this.timeState.time % 3600 % 60);
+        this.timeState.minutes = Math.floor(this.timeState.time % 3600 / 60);
+        this.timeState.hours = Math.floor(this.timeState.time / 3600);
+      }
+    });
+  }
+}
